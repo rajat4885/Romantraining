8553ac84c65d7517d4716f7774c234826265db13
Rajat <rajat4885@gmail.com>
2025-04-05 23:15:27 +0530

    Initial push

---
+++ archive.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Romantrainings
+ */
+
+get_header();
+?>
+
+<?php
+get_footer();
